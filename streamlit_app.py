f47bd533026910797e196d900efa947892009ee3
danikagupta <74515001+danikagupta@users.noreply.github.com>
2025-03-05 20:43:16 +0000

    reorganize code

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,37 +8,40 @@ from langchain_xai import ChatXAI
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_groq import ChatGroq
 
-
 from pydantic.v1 import BaseModel
 from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, AIMessage, ChatMessage
 
 import pandas as pd
 
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-os.environ["ANTHROPIC_API_KEY"] = st.secrets["ANTHROPIC_API_KEY"]
-os.environ["GOOGLE_API_KEY"]=st.secrets["GEMINI_API_KEY"]
-os.environ["XAI_API_KEY"]=st.secrets["XAI_API_KEY"]
-os.environ["HUGGINGFACEHUB_API_TOKEN"]=st.secrets["HF_TOKEN"]
-os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
-
-models={
-
-    "gpt-4o-mini": ChatOpenAI(model="gpt-4o-mini"),
-    "claude-sonnet": ChatAnthropic(model="claude-3-5-sonnet-20240620"),
-    "gemini-1.5-pro": ChatGoogleGenerativeAI(model="gemini-1.5-pro"),
-
-    "grok-2-latest": ChatXAI(model="grok-2-latest"),
-    #"llama": HuggingFaceEndpoint(repo_id="meta-llama/Llama-2-70b-chat-hf",task="text-generation",max_new_tokens=512,temperature=0.7),
-    "llama":ChatGroq(model="llama-3.3-70b-versatile"),
-    "deepseek":ChatGroq(model="deepseek-r1-distill-llama-70b"),
-    
-    #"gpt-4o": ChatOpenAI(model="gpt-4o"),
-    #"claude-haiku": ChatAnthropic(model="claude-3-haiku-20240307"),
-
-}
-
 PROMPT_FILE='data/prompts.csv'
 RESULT_FILE='data/results.csv'
+
+def initialize_models():
+
+    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+    os.environ["ANTHROPIC_API_KEY"] = st.secrets["ANTHROPIC_API_KEY"]
+    os.environ["GOOGLE_API_KEY"]=st.secrets["GEMINI_API_KEY"]
+    os.environ["XAI_API_KEY"]=st.secrets["XAI_API_KEY"]
+    os.environ["HUGGINGFACEHUB_API_TOKEN"]=st.secrets["HF_TOKEN"]
+    os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
+
+    models={
+
+        "gpt-4o-mini": ChatOpenAI(model="gpt-4o-mini"),
+        "claude-sonnet": ChatAnthropic(model="claude-3-5-sonnet-20240620"),
+        "gemini-1.5-pro": ChatGoogleGenerativeAI(model="gemini-1.5-pro"),
+
+        "grok-2-latest": ChatXAI(model="grok-2-latest"),
+        #"llama": HuggingFaceEndpoint(repo_id="meta-llama/Llama-2-70b-chat-hf",task="text-generation",max_new_tokens=512,temperature=0.7),
+        "llama":ChatGroq(model="llama-3.3-70b-versatile"),
+        "deepseek":ChatGroq(model="deepseek-r1-distill-llama-70b"),
+        
+        #"gpt-4o": ChatOpenAI(model="gpt-4o"),
+        #"claude-haiku": ChatAnthropic(model="claude-3-haiku-20240307"),
+
+    }
+    return models
+
 
 def apply_model(model,user_input):
     try:
@@ -65,45 +68,7 @@ def read_file_from_ui_or_fs():
             return df
     return None
 
-
-st.title("Sentio")
-update_ui=st.empty()
-df = read_file_from_ui_or_fs()
-    
-if df is not None:
-    with st.sidebar.expander("Prompts"):
-        st.dataframe(df, hide_index=True)
-    options = st.sidebar.multiselect('Models',options=list(models.keys()),default=list(models.keys()))
-    model_list={key:models[key] for key in options}
-    run_count=st.sidebar.number_input("Runs",min_value=1,max_value=100, value=1)
-    if st.sidebar.button("Run"):
-        with st.spinner("Running...", show_time=True):
-            update_ui.write(f"Starting run with {run_count=} for {models=}")
-            current_count=0
-            total_count=run_count*len(model_list)*df.shape[0]
-            progress_bar=st.progress(current_count)
-            results=[]
-            for i in range(1,run_count+1):
-                for model_choice in model_list.keys():
-                    model_selected=models[model_choice]
-
-                    for index,row in df.iterrows():
-                        user_input=row['Prompt']
-                        update_ui.write(f"Completed {current_count}/{total_count} steps. {i=}, {model_choice=}, {user_input=}")
-                        rsp = apply_model(model_selected,user_input)
-                        results.append({'model':model_choice,'prompt':user_input,'response':rsp})
-                        current_count+=1
-                        progress_bar.progress( (1.0*current_count) / total_count)
-                        #st.write(f"Record: {index=},{user_input=},{rsp=}")
-            update_ui.write(f"All done!!")
-            result_df=pd.DataFrame(results)
-            st.dataframe(result_df,hide_index=True)
-            if current_count>0:
-                if os.path.exists(RESULT_FILE):
-                    result_df.to_csv(RESULT_FILE,mode='a', index=False, header=False)
-                else:
-                    result_df.to_csv(RESULT_FILE, index=False)
-
+def show_download_sidebar():
     with st.sidebar:
         with open(RESULT_FILE, "rb") as file:
             file_bytes = file.read()
@@ -113,5 +78,56 @@ if df is not None:
             file_name="complete_set.csv",
             mime="text/csv",
         )
+
+def save_results(count,result_df):
+    if count>0:
+        if os.path.exists(RESULT_FILE):
+            result_df.to_csv(RESULT_FILE,mode='a', index=False, header=False)
+        else:
+            result_df.to_csv(RESULT_FILE, index=False)
+
+def run_all_models(df,model_list,run_count):
+    update_ui.write(f"Starting run with {run_count=} for {models=}")
+    current_count=0
+    total_count=run_count*len(model_list)*df.shape[0]
+    progress_bar=st.progress(current_count)
+    results=[]
+
+    with st.spinner("Running...", show_time=True):
+        for i in range(1,run_count+1):
+            for model_choice in model_list.keys():
+                for _,row in df.iterrows():
+                    user_input=row['Prompt']
+                    update_ui.write(f"Completed {current_count}/{total_count} steps. {i=}, {model_choice=}, {user_input=}")
+                    rsp = apply_model(models[model_choice],user_input)
+                    results.append({'model':model_choice,'prompt':user_input,'response':rsp})
+                    current_count+=1
+                    progress_bar.progress( (1.0*current_count) / total_count)
+
+    update_ui.write(f"All done!!")
+    result_df=pd.DataFrame(results)
+    st.dataframe(result_df,hide_index=True)
+    save_results(current_count,result_df)
+
+#
+# Main
+#
+
+st.title("Sentio")
+update_ui=st.empty()
+df = read_file_from_ui_or_fs()
+models=initialize_models()
+show_download_sidebar()
+    
+if df is not None:
+    with st.sidebar.expander("Prompts"):
+        st.dataframe(df, hide_index=True)
+    options = st.sidebar.multiselect('Models',options=list(models.keys()),default=list(models.keys()))
+    model_list={key:models[key] for key in options}
+    run_count=st.sidebar.number_input("Runs",min_value=1,max_value=100, value=1)
+    if st.sidebar.button("Run"):
+        run_all_models(df,model_list,run_count)
+
+
   
 
